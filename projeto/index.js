f2b5c5752e48bd004cd3cf069a68cf5315a2c49c
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-12-09 19:02:08 -0300

    sending index.js

--- projeto/index.js
+++ projeto/index.js
@@ -14,7 +14,6 @@ const patient4 = new Patient(7, 'Nathalia', '81912349876', 59.5, 1.58);
 const patient5 = new Patient(8, 'Sandra', '81912349876', 68, 1.6);
 const patient6 = new Patient(9, 'Gabriela', '81912349876', 65, 1.56);
 const patient7 = new Patient(10, 'Fernanda', '81912349876', 58, 1.51);
-const patient8 = new Patient(11, 'Amanda', '81912349876', 63, 1.68);
 
 //Registering Schedules
 const schedule = new Schedule(1, 'Ana', 'Clinic', '09/12/22');
@@ -29,7 +28,6 @@ schedule.verifyScheduleNutri(nutri, '16:00', patient4);
 schedule2.verifyScheduleNutri(nutri2, '10:00', patient5);
 schedule2.verifyScheduleNutri(nutri2, '14:00', patient6);
 schedule2.verifyScheduleNutri(nutri2, '16:00', patient7);
-schedule2.verifyScheduleNutri(nutri2, '16:30', patient8);
 
 console.log(schedule);
 console.log(schedule2);
